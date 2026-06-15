9cc22f5d5737dbf6aae058098d9664ecabc28b26
Benoît Tessier <tessier.benoit28@gmail.com>
2026-06-15 17:05:52 +0200

    feat error page

--- kasa-react/src/pages/Error/index.jsx
+++ kasa-react/src/pages/Error/index.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom'
-
 import './Error.css'
 
 function Error() {
     return (
         <header className='error'>
             <div className='error-content'>
+                <h1 className='error-title'>404</h1>
+                <h2>Oups! La page que vous demandez n'existe pas.</h2>
+                <Link to='/' className='error-link'>
+                    Retourner sur la page d'accueil
+                </Link>
             </div>
         </header>
     )
